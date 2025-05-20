df8ba2fdf74df976c2f883d0421273872ce1a54b
Julio Cesar Gutierrez Rojas <20203tn160@utez.edu.mx>
2025-05-20 16:00:43 -0600

    creacion del server

---
+++ index.js
@@ -0,0 +1,15 @@
+// Importar Express
+const express = require('express');
+const cors = require('cors')
+
+// Inicializar la aplicación Express
+const app = express();
+app.use(cors({ origin: '*' }))
+
+// Definir el puerto para el servidor
+const PORT = 4000;
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
